5a18a22edcd3f0f1de3772215495e08bb8f4454e
NMProgram <1124834@hr.nl>
2026-04-19 14:49:17 +0200

    Added Access Unit Tests for SongLogic and ComposerLogic

---
+++ Code/UnitTests/AccessTests/ComposerLogicTests.cs
@@ -0,0 +1,269 @@
+using System.Data;
+
+namespace DatabaseTests;
+
+[TestClass]
+public sealed class ComposerLogicTests : TestStartup
+{
+    [TestMethod]
+    public void AddUpdateDelete_ComposerLogic_ReturnsComposerOrNull()
+    {
+        // Arrange
+        Composer comp = new(0, "-", "2020-10-10 00:00:00", 2000, "", 1);
+        // Act
+        _cAccess.Add(comp);
+        Composer? foundComp = _cAccess.GetByID("-");
+        // Assert
+        Assert.AreEqual(foundComp, comp);
+        // Act
+        foundComp?.SetName("*");
+        _cAccess.Update(comp, foundComp!);
+        Composer? newComp = _cAccess.GetByID("*");
+        Composer? notAnymore = _cAccess.GetByID("-");
+        // Assert
+        Assert.AreEqual(newComp, foundComp);
+        Assert.IsNull(notAnymore);
+        // Act
+        _cAccess.Delete(newComp.ID);
+        Composer? notHere = _cAccess.GetByID("*");
+        // Assert
+        Assert.IsNull(notHere);
+    }
+    [TestMethod]
+    [DataRow("Aydin-Jewelz123")]
+    [DataRow("Fantomenk")]
+    [DataRow("KDrew")]
+    public void GetByID_ComposerLogic_ReturnsComposerWithOneSong(string name)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        Composer? comp = _cAccess.GetByID(name);
+        // Assert
+        Assert.IsNotNull(comp);
+        Assert.HasCount(1, comp.Songs);
+    }
+    [TestMethod]
+    [DataRow("Waterflame")]
+    public void GetByID_ComposerLogic_ReturnsComposerWithMultipleSongs(string name)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        Composer? comp = _cAccess.GetByID(name);
+        // Assert
+        Assert.IsNotNull(comp);
+        Assert.HasCount(2, comp.Songs);
+    }
+    [TestMethod]
+    [DataRow("")]
+    [DataRow("Nothing")]
+    public void GetByID_ComposerLogic_ReturnsNull(string name)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        Composer? comp = _cAccess.GetByID(name);
+        // Assert
+        Assert.IsNull(comp);
+    }
+    [TestMethod]
+    [DataRow("Fantomenk")]
+    [DataRow("Wat")]
+    [DataRow("asus")]
+    public void GetComposerMatches_ComposerLogic_ReturnsCompOrComps(string inp)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Composer> comps = _cAccess.GetComposerMatches(inp);
+        // Assert
+        Assert.IsNotEmpty(comps);
+        CollectionAssert.AllItemsAreNotNull(comps.ToArray());
+        Assert.IsNotEmpty(comps.First().Songs);
+    }
+    [TestMethod]
+    [DataRow("a", "z")]
+    [DataRow("Albert", "Sean")]
+    [DataRow("helpegasus", "HelpegasuS")]
+    public void GetBetweenCompData_Names_ReturnsCompsOrComp(string low, string high)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Composer> comps = _cAccess.GetBetweenCompData(low, high);
+        // Assert
+        Assert.IsNotEmpty(comps);
+        CollectionAssert.AllItemsAreNotNull(comps.ToArray());
+        Assert.IsNotEmpty(comps.First().Songs);
+    }
+    [TestMethod]
+    [DataRow("zzzzzz", "zzzzzz")]
+    [DataRow("\\", "\\")]
+    public void GetBetweenCompData_Names_ReturnsEmpty(string low, string high)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Composer> comps = _cAccess.GetBetweenCompData(low, high);
+        // Assert
+        Assert.IsEmpty(comps);
+    }
+    [TestMethod]
+    [DataRow("2000-12-20", "2015-04-04")]
+    [DataRow("2010-02-02", "2011-04-13")]
+    [DataRow("2008-10-26", "2008-10-26")]
+    public void GetBetweenCompData_Dates_ReturnsCompsOrComp(string low, string high)
+    {
+        // ^^^ Arrange ^^^
+        DateTime start = DateTime.Parse(low);
+        DateTime end = DateTime.Parse(high);
+        // Act
+        IEnumerable<Composer> comps = _cAccess.GetBetweenCompData(start, end);
+        // Assert
+        Assert.IsNotEmpty(comps);
+        CollectionAssert.AllItemsAreNotNull(comps.ToArray());
+        Assert.IsNotEmpty(comps.First().Songs);
+    }
+    [TestMethod]
+    [DataRow("1980-10-13", "1981-03-16")]
+    [DataRow("2008-10-25", "2008-10-24")]
+    public void GetBetweenCompData_Dates_ReturnsEmpty(string low, string high)
+    {
+        // ^^^ Arrange ^^^
+        DateTime start = DateTime.Parse(low);
+        DateTime end = DateTime.Parse(high);
+        // Act
+        IEnumerable<Composer> comps = _cAccess.GetBetweenCompData(start, end);
+        // Assert
+        Assert.IsEmpty(comps);
+    }
+    [TestMethod]
+    [DataRow(18, 35)]
+    [DataRow(30, 40)]
+    [DataRow(37, 37)]
+    public void GetBetweenCompData_Age_ReturnsCompsOrComp(long low, long high)
+    {
+        // ^^^ Arrange ^^^
+
+        // Act
+        IEnumerable<Composer> comps = _cAccess.GetBetweenCompData(low, high);
+        // Assert
+        Assert.IsNotEmpty(comps);
+        CollectionAssert.AllItemsAreNotNull(comps.ToArray());
+        Assert.IsNotEmpty(comps.First().Songs);
+    }
+    [TestMethod]
+    [DataRow(1, 1)]
+    [DataRow(-15, 2)]
+    public void GetBetweenCompData_Age_ReturnsEmpty(long low, long high)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Composer> comps = _cAccess.GetBetweenCompData(low, high);
+        // Assert
+        Assert.IsEmpty(comps);
+    }
+    [TestMethod]
+    public void GetUnavailableComposers_ComposerLogic_ReturnsComp()
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Composer> comps = _cAccess.GetUnavailableComposers();
+        // Assert
+        Assert.IsNotEmpty(comps);
+        foreach (var comp in comps) 
+        { Assert.AreEqual(0, comp.OnNewgrounds); }
+        CollectionAssert.AllItemsAreNotNull(comps.ToArray());
+        Assert.HasCount(1, comps);
+    }
+    [TestMethod]
+    [DataRow(2, 598682)]
+    [DataRow(1, 311087)]
+    public void GetBySongID_ComposerLogic_ReturnsCompsOrComp(int count, long id)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Composer> comps = _cAccess.GetBySongID(id);
+        // Assert
+        Assert.IsNotEmpty(comps);
+        foreach (var comp in comps) 
+        { Assert.IsTrue(comp.Songs.All(x => x.ID == id)); }
+        CollectionAssert.AllItemsAreNotNull(comps.ToArray());
+        Assert.HasCount(count, comps);
+    }
+    [TestMethod]
+    [DataRow("circles")]
+    [DataRow("Electroman")]
+    [DataRow("Tin")]
+    public void GetBySongName_ComposerLogic_ReturnsCompsOrComp(string name)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Composer> comps = _cAccess.GetBySongName(name);
+        // Assert
+        Assert.IsNotEmpty(comps);
+        foreach (var comp in comps) 
+        { Assert.IsNotNull(comp.Songs.FirstOrDefault(x => x.Name.Contains(name))); }
+        CollectionAssert.AllItemsAreNotNull(comps.ToArray());
+        Assert.IsNotEmpty(comps.First().Songs);
+    }
+    [TestMethod]
+    [DataRow(true, "Waterflame")]
+    [DataRow(true, "Fantomenk")]
+    [DataRow(true, "KDrew")]
+    [DataRow(false, "Invalid")]
+    public void IsInDatabase_ComposerLogic_ReturnsTuple(bool exp, string name)
+    {
+        // ^^^ Arrange ^^^
+        
+        // Act
+        var (res, val, err) = _cAccess.IsInDatabase(name);
+        // Assert
+        Assert.AreEqual(exp, res);
+        Assert.AreEqual(exp, !string.IsNullOrEmpty(val));
+        Assert.AreEqual(exp, err is null);
+    }
+    [TestMethod]
+    [DataRow(false, "Waterflame")]
+    [DataRow(false, "Fantomenk")]
+    [DataRow(false, "KDrew")]
+    [DataRow(true, "Invalid")]
+    public void IsNotInDatabase_ComposerLogic_ReturnsTuple(bool exp, string name)
+    {
+        // ^^^ Arrange ^^^
+        
+        // Act
+        var (res, val, err) = _cAccess.IsNotInDatabase(name);
+        // Assert
+        Assert.AreEqual(exp, res);
+        Assert.AreEqual(exp, !string.IsNullOrEmpty(val));
+        Assert.AreEqual(exp, err is null);
+    }
+    [TestMethod]
+    [DataRow(true, "Waterflame", 643474)]
+    [DataRow(true, "Fantomenk", 386900)]
+    [DataRow(true, "KDrew", 311087)]
+    [DataRow(false, "Helpegasus", 598682)]
+    public void IsNewSong_ComposerLogic_ReturnsTuple(bool exp, string name, long id)
+    {
+        // ^^^ Arrange ^^^
+        
+        // Act
+        var (res, val, err) = _cAccess.IsNewSong(name, id);
+        // Assert
+        Assert.AreEqual(exp, res);
+        Assert.AreEqual(exp, val != -1);
+        Assert.AreEqual(exp, err is null);
+    }
+    [TestMethod]
+    [DataRow(false, "Waterflame", 643474)]
+    [DataRow(false, "Fantomenk", 386900)]
+    [DataRow(false, "KDrew", 311087)]
+    [DataRow(true, "Helpegasus", 598682)]
+    public void IsNotNewSong_ComposerLogic_ReturnsTuple(bool exp, string name, long id)
+    {
+        // ^^^ Arrange ^^^
+        
+        // Act
+        var (res, val, err) = _cAccess.IsNotNewSong(name, id);
+        // Assert
+        Assert.AreEqual(exp, res);
+        Assert.AreEqual(exp, val != -1);
+        Assert.AreEqual(exp, err is null);
+    }
+}
---
+++ Code/UnitTests/AccessTests/SongLogicTests.cs
@@ -0,0 +1,315 @@
+﻿using System.Data;
+
+namespace DatabaseTests;
+
+[TestClass]
+public sealed class SongLogicTests : TestStartup
+{
+    [TestMethod]
+    public void AddUpdateDelete_SongLogic_ReturnsSongOrNull()
+    {
+        // Arrange
+        Song song = new(int.MaxValue, "Nonexistant", "2020-10-10 00:00:00", 15, -50, 1, []);
+        // Act
+        _sAccess.Add(song);
+        Song? foundSong = _sAccess.GetByID(int.MaxValue);
+        // Assert
+        Assert.AreEqual(foundSong, song);
+        // Act
+        foundSong?.SetID(long.MaxValue);
+        _sAccess.Update(song, foundSong!);
+        Song? newSong = _sAccess.GetByID(long.MaxValue);
+        Song? notAnymore = _sAccess.GetByID(int.MaxValue);
+        // Assert
+        Assert.AreEqual(newSong, foundSong);
+        Assert.IsNull(notAnymore);
+        // Act
+        _sAccess.Delete(newSong!);
+        Song? notHere = _sAccess.GetByID(long.MaxValue);
+        // Assert
+        Assert.IsNull(notHere);
+    }
+    [TestMethod]
+    [DataRow(311087)]
+    [DataRow(386900)]
+    [DataRow(643474)]
+    public void GetByID_SongLogic_ReturnsSongWithOneComp(long id)
+    {
+        // ^^^ Arrange ^^^
+        AccessLogic logic = new(Factory);
+        // Act
+        Song? song = _sAccess.GetByID(id);
+        // Assert
+        Assert.IsNotNull(song);
+        Assert.HasCount(1, song.Composers);
+    }
+    [TestMethod]
+    [DataRow(586990)]
+    [DataRow(598682)]
+    public void GetByID_SongLogic_ReturnsSongWithMultipleComps(long id)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        Song? song = _sAccess.GetByID(id);
+        // Assert
+        Assert.IsNotNull(song);
+        Assert.HasCount(2, song.Composers);
+    }
+    [TestMethod]
+    [DataRow(1)]
+    [DataRow(-1)]
+    public void GetByID_SongLogic_ReturnsNull(long id)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        Song? song = _sAccess.GetByID(id);
+        // Assert
+        Assert.IsNull(song);
+    }
+    [TestMethod]
+    [DataRow(311087, -999999)]
+    [DataRow(643474, 635000)]
+    [DataRow(478433, 450000)]
+    public void GetClosestMatch_SongLogic_ReturnsSong(long exp, long id)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        Song? song = _sAccess.GetClosestMatch(id);
+        // Assert
+        Assert.IsNotNull(song);
+        Assert.AreEqual(exp, song.ID);
+    }
+    [TestMethod]
+    [DataRow("Fantomenk")]
+    [DataRow("Soulwind")]
+    [DataRow("circ")]
+    public void GetSongMatches_SongLogic_ReturnsSongOrSongs(string inp)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetSongMatches(inp);
+        // Assert
+        Assert.IsNotEmpty(songs);
+        CollectionAssert.AllItemsAreNotNull(songs.ToArray());
+        Assert.IsNotEmpty(songs.First().Composers);
+    }
+    [TestMethod]
+    [DataRow(1, 999999)]
+    [DataRow(100000, 200000)]
+    [DataRow(188711, 188711)]
+    public void GetBetweenLevelIDs_SongLogic_ReturnsSongsOrSong(long low, long high)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetBetweenLevelIDs(low, high);
+        // Assert
+        Assert.IsNotEmpty(songs);
+        CollectionAssert.AllItemsAreNotNull(songs.ToArray());
+        Assert.IsNotEmpty(songs.First().Composers);
+    }
+    [TestMethod]
+    [DataRow(-int.MaxValue, -100000)]
+    [DataRow(int.MaxValue, long.MaxValue)]
+    public void GetBetweenLevelIDs_SongLogic_ReturnsEmpty(long low, long high)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetBetweenLevelIDs(low, high);
+        // Assert
+        Assert.IsEmpty(songs);
+    }
+    [TestMethod]
+    [DataRow(1, int.MaxValue)]
+    [DataRow(503241, 725205)]
+    [DataRow(598682, 598682)]
+    public void GetBetweenSongData_IDs_ReturnsSongsOrSong(long low, long high)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetBetweenSongData(low, high);
+        // Assert
+        Assert.IsNotEmpty(songs);
+        CollectionAssert.AllItemsAreNotNull(songs.ToArray());
+        Assert.IsNotEmpty(songs.First().Composers);
+    }
+    [TestMethod]
+    [DataRow(-99999, 0)]
+    [DataRow(4329843290, 5439053354)]
+    public void GetBetweenSongData_IDs_ReturnsEmpty(long low, long high)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetBetweenSongData(low, high);
+        // Assert
+        Assert.IsEmpty(songs);
+    }
+    [TestMethod]
+    [DataRow("a", "z")]
+    [DataRow("Albert", "Gottfried")]
+    [DataRow("-", "/")]
+    [DataRow("Tiny Tunes", "Tiny Tunes")]
+    public void GetBetweenSongData_Names_ReturnsSongsOrSong(string low, string high)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetBetweenSongData(low, high);
+        // Assert
+        Assert.IsNotEmpty(songs);
+        CollectionAssert.AllItemsAreNotNull(songs.ToArray());
+        Assert.IsNotEmpty(songs.First().Composers);
+    }
+    [TestMethod]
+    [DataRow("zzzzzz", "zzzzzz")]
+    [DataRow("\\", "\\")]
+    public void GetBetweenSongData_Names_ReturnsEmpty(string low, string high)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetBetweenSongData(low, high);
+        // Assert
+        Assert.IsEmpty(songs);
+    }
+    [TestMethod]
+    [DataRow("2000-12-20", "2015-04-04")]
+    [DataRow("2010-02-02", "2011-04-13")]
+    [DataRow("2010-02-12", "2010-02-12")]
+    public void GetBetweenSongData_Dates_ReturnsSongsOrSong(string low, string high)
+    {
+        // ^^^ Arrange ^^^
+        DateTime start = DateTime.Parse(low);
+        DateTime end = DateTime.Parse(high);
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetBetweenSongData(start, end);
+        // Assert
+        Assert.IsNotEmpty(songs);
+        CollectionAssert.AllItemsAreNotNull(songs.ToArray());
+        Assert.IsNotEmpty(songs.First().Composers);
+    }
+    [TestMethod]
+    [DataRow("1980-10-13", "1981-03-16")]
+    [DataRow("2008-10-25", "2008-10-24")]
+    public void GetBetweenSongData_Dates_ReturnsEmpty(string low, string high)
+    {
+        // ^^^ Arrange ^^^
+        DateTime start = DateTime.Parse(low);
+        DateTime end = DateTime.Parse(high);
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetBetweenSongData(start, end);
+        // Assert
+        Assert.IsEmpty(songs);
+    }
+    [TestMethod]
+    [DataRow("Video Game")]
+    [DataRow("Dubstep")]
+    [DataRow("Dance")]
+    public void GetByGenre_SongLogic_ReturnsSongsOrSong(string genre)
+    {
+        // ^^^ Arrange ^^^
+        var (_, val, _) = InputLogic.IsValidGenre(genre);
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetByGenre(val);
+        // Assert
+        Assert.IsNotEmpty(songs);
+        foreach (var song in songs) { Assert.AreEqual(val, song.Genre); }
+        CollectionAssert.AllItemsAreNotNull(songs.ToArray());
+        Assert.IsNotEmpty(songs.First().Composers);
+    }
+    [TestMethod]
+    [DataRow("Bluegrass")]
+    public void GetByGenre_SongLogic_ReturnsEmpty(string genre)
+    {
+        // ^^^ Arrange ^^^
+        var (_, val, _) = InputLogic.IsValidGenre(genre);
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetByGenre(val);
+        // Assert
+        Assert.IsEmpty(songs);
+    }
+    [TestMethod]
+    [DataRow("Fantomenk")]
+    [DataRow("Waterflame")]
+    [DataRow("Ay")]
+    public void GetSongsFromComposer_SongLogic_ReturnsSongsOrSong(string comp)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetSongsFromComposer(comp);
+        // Assert
+        Assert.IsNotEmpty(songs);
+        foreach (var song in songs) 
+        { Assert.IsNotNull(song.Composers.FirstOrDefault(x => x.Name.Contains(comp))); }
+        CollectionAssert.AllItemsAreNotNull(songs.ToArray());
+        Assert.IsNotEmpty(songs.First().Composers);
+    }
+    [TestMethod]
+    [DataRow("NNWNFKWIJOFWIJOK")]
+    public void GetBetweenSongData_IDs_ReturnsEmpty(string comp)
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetSongsFromComposer(comp);
+        // Assert
+        Assert.IsEmpty(songs);
+    }
+    [TestMethod]
+    public void GetUnavailableSongs_SongLogic_ReturnsSongs()
+    {
+        // ^^^ Arrange ^^^
+        // Act
+        IEnumerable<Song> songs = _sAccess.GetUnavailableSongs();
+        // Assert
+        Assert.IsNotEmpty(songs);
+        foreach (var song in songs) 
+        { Assert.AreEqual(0, song.Available); }
+        CollectionAssert.AllItemsAreNotNull(songs.ToArray());
+        Assert.HasCount(2, songs.First().Composers);
+    }
+    [TestMethod]
+    [DataRow(true, 598682)]
+    [DataRow(true, 311087)]
+    [DataRow(true, 643474)]
+    [DataRow(false, -1)]
+    public void IsInDatabase_SongLogic_ReturnsTuple(bool exp, long id)
+    {
+        // ^^^ Arrange ^^^
+        
+        // Act
+        var (res, val, err) = _sAccess.IsInDatabase(id);
+        // Assert
+        Assert.AreEqual(exp, res);
+        Assert.AreEqual(exp, val != -1);
+        Assert.AreEqual(exp, err is null);
+    }
+    [TestMethod]
+    [DataRow(true, 1)]
+    [DataRow(true, 0)]
+    [DataRow(true, 49320429)]
+    [DataRow(false, 598682)]
+    public void IsNotInDatabase_SongLogic_ReturnsTuple(bool exp, long id)
+    {
+        // ^^^ Arrange ^^^
+        
+        // Act
+        var (res, val, err) = _sAccess.IsNotInDatabase(id);
+        // Assert
+        Assert.AreEqual(exp, res);
+        Assert.AreEqual(exp, val != -1);
+        Assert.AreEqual(exp, err is null);
+    }
+    [TestMethod]
+    [DataRow(true, 1)]
+    [DataRow(true, 1700)]
+    [DataRow(true, int.MaxValue)]
+    [DataRow(false, 99719)]
+    public void IsUniqueLevelID_SongLogic_ReturnsTuple(bool exp, long levelID)
+    {
+        // ^^^ Arrange ^^^
+        
+        // Act
+        var (res, val, err) = _sAccess.IsUniqueLevelID(levelID);
+        // Assert
+        Assert.AreEqual(exp, res);
+        Assert.AreEqual(exp, val != -1);
+        Assert.AreEqual(exp, err is null);
+    }
+}

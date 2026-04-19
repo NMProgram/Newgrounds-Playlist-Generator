ce86fe08c7f146656e11f3653c0149316818ec02
NMProgram <1124834@hr.nl>
2026-04-19 14:54:01 +0200

    Updated location of constructor and code of CloseStream() method in PathAbstraction

--- Code/Project/Data/DataModel/PathAbstraction.cs
+++ Code/Project/Data/DataModel/PathAbstraction.cs
@@ -1,14 +1,17 @@
 // used to avoid using paths with TagLib.File.Create
 public class PathAbstraction : TagLib.File.IFileAbstraction
 {
+    public string Name { get; }
+    public Stream ReadStream { get; }
+    public Stream WriteStream { get; }
     public PathAbstraction(string name, Stream stream)
     {
         Name = name;
         ReadStream = stream;
         WriteStream = stream;
     }
-    public string Name { get; }
-    public Stream ReadStream { get; }
-    public Stream WriteStream { get; }
-    public void CloseStream(Stream stream) { }
+    public void CloseStream(Stream stream)
+    {
+        stream.Dispose();
+    }
 }

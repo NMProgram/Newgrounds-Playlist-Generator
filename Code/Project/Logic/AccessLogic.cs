daf61a9e3f0023d9bda7567ae1ef0f95b12f5ecf
NMProgram <1124834@hr.nl>
2026-04-21 19:49:12 +0200

    Refactored AccessLogic to be less dependent on a complex generic method

--- Code/Project/Logic/AccessLogic.cs
+++ Code/Project/Logic/AccessLogic.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Security.Cryptography.X509Certificates;
 
-public class AccessLogic
+public abstract class AccessLogic<TKey, TValue>
 {
     readonly protected SongAccess _sAccess;
     readonly protected ComposerAccess _cAccess;
@@ -12,20 +12,6 @@ public class AccessLogic
         _cAccess = new(con);
         _scAccess = new(con);
     }
-    [ExcludeFromCodeCoverage]
-    public void Update<T>(T value1, T value2)
-    {
-        switch((value1, value2))
-        {
-            case (Song s1, Song s2): Update(s1, s2); break;
-            case (Composer c1, Composer c2): Update(c1, c2); break;
-        }
-    }
-    [ExcludeFromCodeCoverage]
-    public object? GetByID<TKey>(TKey value) => value switch
-    {
-        long s => GetByID(s),
-        string c => GetByID(c),
-        _ => throw new ArgumentException($"Invalid ID type \'{typeof(TKey)}\'.", nameof(value))
-    };
+    public abstract TValue? GetByID(TKey key);
+    public abstract void Update(TValue o, TValue n);
 }
